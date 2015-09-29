7386dcdbac721c72f05be7f3cb5dc4734aadae71
alexisjanvier <contact@alexisjanvier.net>
2015-09-29 23:42:01 +0200

    Working version, but must be improve

--- src/Stack/DocGenerator/DocGenerator.php
+++ src/Stack/DocGenerator/DocGenerator.php
@@ -14,15 +14,59 @@ class DocGenerator implements HttpKernelInterface
     public function __construct(HttpKernelInterface $app, array $options = [])
     {
         $this->app = $app;
-        $this->definition = $request->attributes->get('raml.config');
     }
 
     public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
     {
+        $this->definition = $request->attributes->get('raml.config');
         $response = $this->app->handle($request, $type, $catch);
         // Render twig views/doc.html.twig, genereted with webpack from src, with definition as parameter
-        $response = new Response('API DOCUMENTATION');
+
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/views');
+        $twig = new \Twig_Environment($loader);
+        $description = $this->getFormatedDescription();
+        $html = $twig->render('doc.html.twig', $description);
+        $response = new Response($html);
         return $response;
+    }
+
+    private function getFormatedDescription ()
+    {
+        $resources = array();
+        foreach ($this->definition->getResources() as $resource) {
+            $endpoints = array();
+            foreach ($resource->getMethods() as $method) {
+                $endpoints[] = [
+                    'uri' => $resource->getUri(),
+                    'method' => $method->getType(),
+                    'description' => $method->getDescription(),
+                ];
+            }
+
+            foreach ($resource->getResources() as $endpoint) {
+                foreach ($endpoint->getMethods() as $method) {
+                    $endpoints[] = [
+                        'uri' => $endpoint->getUri(),
+                        'method' => $method->getType(),
+                        'description' => $method->getDescription(),
+                    ];
+                }
+            }
+
+            $resources[] = [
+                'uri' => $resource->getUri(),
+                'name' => $resource->getDisplayName(),
+                'description' => $resource->getDescription(),
+                'endpoints' => $endpoints,
+            ];
+        }
+
+        return array(
+            'apiName' => $this->definition->getTitle(),
+            'resources' => $resources,
+            'baseUrl' => $this->definition->getBaseUrl(),
+            'version' => $this->definition->getVersion(),
+        );
     }
 
     /**
